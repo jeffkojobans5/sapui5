8ddb8ce3b473df91df776d0128b297b32e022768
Jeff Bans <iambansah@gmail.com>
2024-06-07 04:28:52 +0000

    Container Shell

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,24 @@
+// sap.ui.define([
+// 	"sap/ui/core/mvc/XMLView"
+// ], (XMLView) => {
+// 	"use strict";
+
+// 	XMLView.create({
+// 		viewName: "ui5.walkthrough.view.App"
+// 	}).then((oView) => oView.placeAt("content"));
+// });
+
+
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], (XMLView) => {
+	"sap/ui/core/ComponentContainer"
+], (ComponentContainer) => {
 	"use strict";
 
-	XMLView.create({
-		viewName: "ui5.walkthrough.view.App"
-	}).then((oView) => oView.placeAt("content"));
+	new ComponentContainer({
+		name: "ui5.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
-
